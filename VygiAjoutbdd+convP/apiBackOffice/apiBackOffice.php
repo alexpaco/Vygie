fd7897174ee76a2a3a049c93ef2d5bb0ea8c61a0
BryanMatera <bryanendo777@gmail.com>
2017-04-25 06:41:00 +0200

    modif back office

--- VygiAjoutbdd+convP/apiBackOffice/apiBackOffice.php
+++ VygiAjoutbdd+convP/apiBackOffice/apiBackOffice.php
@@ -11,10 +11,30 @@ try
 
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == 'GET') {
+  if(isset($_GET['verif']))
+  {
+    if($_GET['verif']=='vrai')
+    {
+      $req= $bdd->prepare("SELECT * FROM villes WHERE nom_Ville LIKE ?");
+      $req->execute(array($_GET['lettre']."%"));
+      $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
+      echo json_encode($resultat);
+    }
+    else
+    {
+      $req= $bdd->prepare("SELECT * FROM villes WHERE nom_Ville = ?");
+      $req->execute(array($_GET['ville']));
+      $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
+      echo json_encode($resultat);
+    }
+  }
+  else
+  {
     $req= $bdd->prepare("SELECT * FROM ecoles");
     $req->execute();
     $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($resultat);
+  }
          		} else if ($method == 'POST'){
        				$input = json_decode(file_get_contents('php://input'),true);
                     $ville = $input['infos']['ville'];
@@ -22,9 +42,7 @@ if ($method == 'GET') {
                     $id_Departement = $input['infos']['id_Departement'];
                     $nom_Ecole = $input['infos']['nom_Ecole'];
                     $adresse_Ecole = $input['infos']['adresse_Ecole'];
-                    $longitude = $input['infos']['longitude'];
-                    $latitude = $input['infos']['latitude'];
-                    
+                   
                         $query=$bdd->prepare("SELECT id_Ville FROM villes WHERE nom_Ville=?");
                         $query->execute(array($ville));
                         $count = $query->rowCount();
@@ -37,28 +55,23 @@ if ($method == 'GET') {
                         $resultatDep = $dep->fetchAll(PDO::FETCH_ASSOC);
                         // $resultatDep = intval($resultatDep[0]);
                         // var_dump($resultatDep);
+                        $verif = $bdd ->prepare("SELECT nom_Ecole, adresse_Ecole FROM ecoles WHERE nom_Ecole = ?");
+                        $verif->execute(array($nom_Ecole));
+                        $verifs = $verif->rowCount();
+                        $nom = $verif->fetchAll(PDO::FETCH_ASSOC);
 
-                        if($count < 1){ 
-
+                        if ($nom[0]['nom_Ecole'] === $nom_Ecole && $nom[0]['adresse_Ecole'] === $adresse_Ecole){
+                        }
+                        else if($count < 1){ 
                         $insertVille=$bdd->prepare('INSERT INTO villes (codePostal, nom_Ville, id_Departement) VALUES(?,?,?)');
                         $insertVille->execute(array($codePostal, $ville, $resultatDep[0]['id_Departement']));
                         $recup=$bdd->lastInsertId();
-                        $req = $bdd->prepare('INSERT INTO ecoles (nom_Ecole, adresse_Ecole, longitude, latitude,id_Ville) VALUES(?,?,?,?,?)');
-                        $req->execute(array($nom_Ecole, $adresse_Ecole, $longitude, $latitude,$recup));
+                        $req = $bdd->prepare('INSERT INTO ecoles (nom_Ecole, adresse_Ecole, id_Ville) VALUES(?,?,?)');
+                        $req->execute(array($nom_Ecole, $adresse_Ecole, $recup));
                           }else{
-                          $req = $bdd->prepare('INSERT INTO ecoles (nom_Ecole, adresse_Ecole, longitude, latitude,id_Ville) VALUES(?,?,?,?,?)');
-                          $req->execute(array($nom_Ecole, $adresse_Ecole, $longitude, $latitude, $resultatVille[0]['id_Ville']));             
+                          $req = $bdd->prepare('INSERT INTO ecoles (nom_Ecole, adresse_Ecole,id_Ville) VALUES(?,?,?)');
+                          $req->execute(array($nom_Ecole, $adresse_Ecole, $resultatVille[0]['id_Ville']));             
                         }
                 }
-   					   
-
-
-
-   					 //    else if($method == 'DELETE'){
-   						// $nom_Ecole = $input->infos->nom_Ecole;
-    					// $req= $bdd->query("DELETE FROM ecoles WHERE nom_ecole= '$nom_Ecole'");
-    					// // 
-    					// echo $nom_Ecole;
-  							// 		}
-
+ 					 
 ?>
